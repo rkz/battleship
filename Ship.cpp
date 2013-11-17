1771b961b1154c441516e5494ebf34e0b6b2f46b
Raphael Kolm <raphael.kolm@gmail.com>
2013-11-17 22:24:35 +0100

    Nettoyage Ship.h + Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -1,14 +1,9 @@
-/*
- * Ship.cpp
- *
- *  Created on: 17 nov. 2013
- *      Author: benjamin
- */
-#include "Cell.h"
 #include "Ship.h"
 
 #include <cassert>
 using namespace std;
+
+#include "Cell.h"
 
 Ship::Ship(vector<Cell*> _cells, string _name)
 : cells(_cells), name(_name)
@@ -18,21 +13,23 @@ Ship::Ship(vector<Cell*> _cells, string _name)
 		assert(cells[i] != 0);
 }
 
-bool Ship::isSunk() const {
-
-    for(unsigned int i = 0; i < cells.size(); i++){
-        if(cells[i]->getStatus() != TOUCH)
+bool Ship::isSunk() const
+{
+    for (unsigned int i = 0; i < cells.size(); i++) {
+        if (cells[i]->getStatus() != TOUCH)
             return false;
     }
+
     return true;
 }
 
-bool Ship::isTouched() const {
-
-    for(unsigned int i = 0; i < cells.size(); i++){
-        if(cells[i]->getStatus() == TOUCH)
+bool Ship::isTouched() const
+{
+    for (unsigned int i = 0; i < cells.size(); i++) {
+        if (cells[i]->getStatus() == TOUCH)
             return true;
     }
+
     return false;
 }
 
@@ -40,8 +37,8 @@ std::vector<Cell*> Ship::getCells() const
 {
     return cells;
 }
+
 std::string Ship::getName() const
 {
     return name;
 }
-
--- Ship.h
+++ Ship.h
@@ -1,12 +1,14 @@
 /*
  * Ship.h
  *
- * Un ship est l'entité définie par le joueur en début de partie. Il contient des pointeurs vers des cellules.
+ * Un ship est l'entitÃ© dÃ©finie par le joueur en dÃ©but de partie. Il contient des pointeurs vers des cellules.
  */
 
 #ifndef SHIP_H_INCLUDED
 #define SHIP_H_INCLUDED
+
 #include <vector>
+
 #include "Cell.h"
 
 class Ship
@@ -14,14 +16,15 @@ class Ship
 private:
     std::vector<Cell*>  cells;
     std::string name;
+
 public:
     Ship(std::vector<Cell*> cells, std::string name);
+
     bool isSunk() const;
     bool isTouched() const;
+
     std::vector<Cell*> getCells() const;
     std::string getName() const;
 };
-
-
 
 #endif // SHIP_H_INCLUDED
