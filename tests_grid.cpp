0b55c9696f9003e5957c76c889750a9d6c42353b
Raphael Kolm <raphael.kolm@gmail.com>
2013-11-21 23:26:13 +0100

    Cas de tests Grid

--- tests_grid.cpp
+++ tests_grid.cpp
@@ -11,6 +11,21 @@ BOOST_AUTO_TEST_CASE( grid_create )
 {
 	Grid g(10);
 	BOOST_CHECK(g.getSize() == 10);
+
+	BOOST_CHECK( g.isPositionValid(Position(0, 0)) );
+	BOOST_CHECK( g.isPositionValid(Position(9, 9)) );
+	BOOST_CHECK( !g.isPositionValid(Position(10, 10)) );
+}
+
+BOOST_AUTO_TEST_CASE( grid_addShip )
+{
+	Grid g(10);
+
+	BOOST_CHECK( g.addShip(Position(1, 1), HORIZONTAL, 3, "test ship") );
+
+	BOOST_CHECK( g.getShipAtPosition(Position(1, 1)) != 0 );
+	BOOST_CHECK( g.getShipAtPosition(Position(2, 1)) != 0 );
+	BOOST_CHECK( g.getShipAtPosition(Position(3, 1)) != 0 );
 }
 
 #endif
