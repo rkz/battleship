051be16dfb758c3276df298e55435f35dd9401f4
Maxime <maxcartan@club-internet.fr>
2013-11-22 23:58:27 +0100

    Lancement d'une partie dans le main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include "Game.h"
 #include "Grid.h"
 #include "LocalPlayer.h"
 
@@ -15,12 +16,16 @@ int main ()
     g.getCell(Position(2,1))->setStatus(TOUCH); g.getCell(Position(3,1))->setStatus(TOUCH); g.getCell(Position(4,1))->setStatus(TOUCH);
     g.getCell(Position(3,3))->setStatus(WATER);
     cout <<  g;*/
-
-    LocalPlayer p;
-    p.initFleet();
+    
+    /*LocalPlayer p;
+    p.initFleet();*/
+    
+    AbstractPlayer* player1 = new LocalPlayer();
+    AbstractPlayer* player2 = new LocalPlayer();
+    Game game = Game(player1, player2);
+    game.run();
 
 	return 0;
-
 
 }
 
