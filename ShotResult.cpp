7fb621a3be769920d1bc0af5dfc9cf544bdcf585
Maxime <maxcartan@club-internet.fr>
2013-11-22 23:55:10 +0100

    Classe ShotResult

--- ShotResult.cpp
+++ ShotResult.cpp
@@ -1,11 +1,33 @@
 #include "ShotResult.h"
 
-ShotResult::ShotResult()
+ShotResult::ShotResult(Grid _targetGrid, Result _result, bool _winningShot)
+    : targetGrid(_targetGrid), result(_result), winningShot(_winningShot)
 {
 
 }
 
-bool ShotResult::isWinning()
+bool ShotResult::isValid() const
+{
+    return (result != ALREADY_PLAYED);
+}
+
+bool ShotResult::isWinning() const
 {
     return winningShot;
+}
+
+std::string ShotResult::getResultAsString() const
+{
+    switch (result) {
+        case MISSED: return "missed";
+        case TOUCHED: return "touched";
+        case SUNK: return "sunk";
+        case ALREADY_PLAYED: return "position already shot";
+        default: return "";
+    };
+}
+
+void ShotResult::printTargetGrid()
+{
+    std::cout << targetGrid << std::endl;
 }
