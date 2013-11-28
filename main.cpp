b5b5c120bdfc1c91cc5c5259d5b799f1e7e23d7a
Raphael Kolm <raphael.kolm@gmail.com>
2013-11-28 22:24:03 +0100

    Adaptation du main() pour choisir le mode au démarrage

--- main.cpp
+++ main.cpp
@@ -9,27 +9,76 @@
 
 using namespace std;
 
-int main ()
+enum BattleshipMode
 {
-	string name1, name2;
-    cout << "Please type players' names." << endl << endl;
-    cout << "Player 1: ";
-    cin >> name1;
-    cout << "Player 2: ";
-    cin >> name2;
-    cout << endl;
-    system("pause");
+	LOCAL,
+	NETWORK_CLIENT,
+	NETWORK_SERVER
+};
 
-    LocalPlayer* player1 = new LocalPlayer(name1);
-    LocalPlayer* player2 = new LocalPlayer(name2);
+class UnknownMode : public exception {};
 
-    Game game = Game(player1, player2);
-    game.run();
+BattleshipMode getMode (int argc, char** argv)
+{
+	if (argc < 2) throw UnknownMode();
 
-    delete player1;
-    delete player2;
+	string mode(argv[1]);
+	if (mode == "local") return LOCAL;
+	else if (mode == "client") return NETWORK_CLIENT;
+	else if (mode == "server") return NETWORK_SERVER;
+	else throw UnknownMode();
+}
 
-	return 0;
+int main (int argc, char** argv)
+{
+	try
+	{
+		BattleshipMode mode = getMode(argc, argv);
+
+		if (mode == LOCAL)
+		{
+			string name1, name2;
+			cout << "Please type players' names." << endl << endl;
+			cout << "Player 1: ";
+			cin >> name1;
+			cout << "Player 2: ";
+			cin >> name2;
+			cout << endl;
+			system("pause");
+
+			LocalPlayer* player1 = new LocalPlayer(name1);
+			LocalPlayer* player2 = new LocalPlayer(name2);
+
+			Game game = Game(player1, player2);
+			game.run();
+
+			delete player1;
+			delete player2;
+
+			return 0;
+		}
+
+		if (mode == NETWORK_CLIENT)
+		{
+			cout << "Network client mode is not implemented yet!" << endl;
+			return 0;
+		}
+
+		if (mode == NETWORK_SERVER)
+		{
+			cout << "Network server mode is not implemented yet!" << endl;
+			return 0;
+		}
+	}
+	catch (UnknownMode&)
+	{
+		cout << "Usage: battleship <mode>" << endl << endl
+		     << "Supported modes:" << endl
+		     << "    local -- Both players play on this computer." << endl
+		     << "    client -- Network mode / client." << endl
+		     << "    server -- Network mode / server." << endl;
+		exit(1);
+	}
 }
 
 #endif
